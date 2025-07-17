36dbb81fabdae2fba6c00a70e2398c9045be576c
Shah Neil Khan <iamsnkonline@gmail.com>
2025-07-17 05:39:09 -0700

    category_list_pagination_model.dart

---
+++ lib/features/category/model/category_list_pagination_model.dart
@@ -0,0 +1,85 @@
+class CategoryListPaginationModel {
+  int? code;
+  String? status;
+  String? msg;
+  CategoryList? data;
+
+  CategoryListPaginationModel({this.code, this.status, this.msg, this.data});
+
+  CategoryListPaginationModel.fromJson(Map<String, dynamic> json) {
+    code = json['code'];
+    status = json['status'];
+    msg = json['msg'];
+    data = json['data'] != null ? CategoryList.fromJson(json['data']) : null;
+  }
+
+}
+
+class CategoryList {
+  List<CategoryModel>? results;
+  int? total;
+  int? firstPage;
+  int? previous;
+  int? next;
+  int? lastPage;
+
+  CategoryList(
+      {this.results,
+        this.total,
+        this.firstPage,
+        this.previous,
+        this.next,
+        this.lastPage});
+
+  CategoryList.fromJson(Map<String, dynamic> json) {
+    if (json['results'] != null) {
+      results = <CategoryModel>[];
+      json['results'].forEach((v) {
+        results!.add(CategoryModel.fromJson(v));
+      });
+    }
+    total = json['total'];
+    firstPage = json['first_page'];
+    previous = json['previous'];
+    next = json['next'];
+    lastPage = json['last_page'];
+  }
+
+}
+
+class CategoryModel {
+  String? sId;
+  String? title;
+  String? slug;
+  String? description;
+  String? icon;
+  Null parent;
+  String? createdAt;
+  String? updatedAt;
+  int? iV;
+
+  CategoryModel(
+      {this.sId,
+        this.title,
+        this.slug,
+        this.description,
+        this.icon,
+        this.parent,
+        this.createdAt,
+        this.updatedAt,
+        this.iV});
+
+  CategoryModel.fromJson(Map<String, dynamic> json) {
+    sId = json['_id'];
+    title = json['title'];
+    slug = json['slug'];
+    description = json['description'];
+    icon = json['icon'];
+    parent = json['parent'];
+    createdAt = json['createdAt'];
+    updatedAt = json['updatedAt'];
+    iV = json['__v'];
+  }
+
+
+}
