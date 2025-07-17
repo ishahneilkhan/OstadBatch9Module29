913734f0a24d15afe688f8264e45a49dc203be75
Shah Neil Khan <iamsnkonline@gmail.com>
2025-07-17 10:01:21 -0700

    assets_path.dart

---
+++ lib/app/assets_path.dart
@@ -0,0 +1,7 @@
+class AssetsPath{
+  static const String _initialPath='assets/images';
+  static String logo='$_initialPath/logo.svg';
+  static String logoNav='$_initialPath/logo_nav.svg';
+  static String shoe = '$_initialPath/shoe.png';
+
+}
