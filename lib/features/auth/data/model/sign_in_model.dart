1da964a05d691ccc10aa2ca4d1af2319c44d517a
Shah Neil Khan <iamsnkonline@gmail.com>
2025-07-17 10:13:33 -0700

    sign_in_model.dart

---
+++ lib/features/auth/data/model/sign_in_model.dart
@@ -0,0 +1,49 @@
+import 'package:ecomers_app/features/auth/data/model/user_model.dart';
+
+class SignInModel {
+  int? code;
+  String? status;
+  String? msg;
+  Data? data;
+
+  SignInModel({this.code, this.status, this.msg, this.data});
+
+  SignInModel.fromJson(Map<String, dynamic> json) {
+    code = json['code'];
+    status = json['status'];
+    msg = json['msg'];
+    data = json['data'] != null ? Data.fromJson(json['data']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['code'] = code;
+    data['status'] = status;
+    data['msg'] = msg;
+    if (this.data != null) {
+      data['data'] = this.data!.toJson();
+    }
+    return data;
+  }
+}
+
+class Data {
+  String? token;
+  UserModel? user;
+
+  Data({this.token, this.user});
+
+  Data.fromJson(Map<String, dynamic> json) {
+    token = json['token'];
+    user = json['user'] != null ? UserModel.fromJson(json['user']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['token'] = token;
+    if (user != null) {
+      data['user'] = user!.toJson();
+    }
+    return data;
+  }
+}
