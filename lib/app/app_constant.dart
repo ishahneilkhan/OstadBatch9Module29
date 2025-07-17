5ca91e5d756ef2398197c0cacb879f5ebc0e3cf0
Shah Neil Khan <iamsnkonline@gmail.com>
2025-07-17 10:02:42 -0700

    app_constant.dart

---
+++ lib/app/app_constant.dart
@@ -0,0 +1,6 @@
+class AppConstant{
+  static const timer =10;
+  static const String specialCatId ='67c35b395e8a445235de197e';
+  static const String popularCatId ='67c35af85e8a445235de197b';
+  static const String newCatId ='67ae2865149a96f2acf9ad72';
+}
